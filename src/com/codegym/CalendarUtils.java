d5144375f5e032d6056672abc69d790c4b0c9b80
Nguyen Khac Nhat <khacnhat.info@gmail.com>
2019-05-08 17:23:18 +0700

    Make variable names bad

--- src/com/codegym/CalendarUtils.java
+++ src/com/codegym/CalendarUtils.java
@@ -2,8 +2,8 @@ package com.codegym;
 
 public class CalendarUtils  {
 
-    public static int getDaysOfMonth(int month, int year) {
-        switch (month) {
+    public static int getDaysOfMonth(int m, int y) {
+        switch (m) {
             case 1:
             case 3:
             case 5:
@@ -18,9 +18,9 @@ public class CalendarUtils  {
             case 11:
                 return 30;
             case 2:
-                if(year % 4 == 0) {
-                    if(year % 100 == 0) {
-                        if(year % 400 == 0) {
+                if(y % 4 == 0) {
+                    if(y % 100 == 0) {
+                        if(y % 400 == 0) {
                             return 29;
                         }
                         return 28;
